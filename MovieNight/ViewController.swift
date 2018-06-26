7e43d7199de431d7b6d4a33d18d0b37d8e8a1dfd
Ali Chegini <ali.c.engineer@gmail.com>
2018-06-26 18:17:27 +0200

    initial commit

---
+++ MovieNight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieNight
+//
+//  Created by Ehsan on 26/06/2018.
+//  Copyright © 2018 Ali C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
